edf02020659a0d4fd1a11af90f4da58ddd76d63c
ABourdarie <alexandre.bourdarie@etu.unilim.fr>
2021-11-08 19:24:27 +0100

    Deuxième Commit, les tests ont été ajoutés avec LegacyInn

--- src/test/java/fr/unilim/iut/gildedrose/InnTest.java
+++ src/test/java/fr/unilim/iut/gildedrose/InnTest.java
@@ -4,7 +4,75 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class InnTest {
+  
+	Inn inn = new Inn();
+  
   @Test
-  public void should_write_tests() {
+  public void should_list_item() {
+	  
+	  
+	  assertThat(inn.getItems()).extracting("name").containsExactly(
+			  "+5 Dexterity Vest",
+			  "Aged Brie", 
+			  "Elixir of the Mongoose", 
+			  "Sulfuras, Hand of Ragnaros", 
+			  "Backstage passes to a TAFKAL80ETC concert", 
+			  "Conjured Mana Cake");
+  
+	  assertThat(inn.getItems()).extracting("sellIn").containsExactly(10, 2, 5, 0, 15, 3);
+	  assertThat(inn.getItems()).extracting("quality").containsExactly(20, 0, 7, 80, 20, 6);
+  }
+  
+  @Test
+  public void should_update_item() {
+	 	  
+	  inn.updateQuality();
+	  assertThat(inn.getItems()).extracting("name").containsExactly(
+			  "+5 Dexterity Vest",
+			  "Aged Brie", 
+			  "Elixir of the Mongoose", 
+			  "Sulfuras, Hand of Ragnaros", 
+			  "Backstage passes to a TAFKAL80ETC concert", 
+			  "Conjured Mana Cake");
+  
+	  assertThat(inn.getItems()).extracting("sellIn").containsExactly(9, 1, 4, 0, 14, 2);
+	  assertThat(inn.getItems()).extracting("quality").containsExactly(19, 1, 6, 80, 21, 5);
+  }
+  
+  @Test
+  public void should_update_item_twice() {
+	 	  
+	  inn.updateQuality();
+	  inn.updateQuality();
+	  
+	  assertThat(inn.getItems()).extracting("name").containsExactly(
+			  "+5 Dexterity Vest",
+			  "Aged Brie", 
+			  "Elixir of the Mongoose", 
+			  "Sulfuras, Hand of Ragnaros", 
+			  "Backstage passes to a TAFKAL80ETC concert", 
+			  "Conjured Mana Cake");
+  
+	  assertThat(inn.getItems()).extracting("sellIn").containsExactly(8, 0, 3, 0, 13, 1);
+	  assertThat(inn.getItems()).extracting("quality").containsExactly(18, 2, 5, 80, 22, 4);
+  }
+  
+  @Test
+  public void should_update_item_trice() {
+	 	  
+	  inn.updateQuality();
+	  inn.updateQuality();
+	  inn.updateQuality();
+	  
+	  assertThat(inn.getItems()).extracting("name").containsExactly(
+			  "+5 Dexterity Vest",
+			  "Aged Brie", 
+			  "Elixir of the Mongoose", 
+			  "Sulfuras, Hand of Ragnaros", 
+			  "Backstage passes to a TAFKAL80ETC concert", 
+			  "Conjured Mana Cake");
+  
+	  assertThat(inn.getItems()).extracting("sellIn").containsExactly(7, -1, 2, 0, 12, 0);
+	  assertThat(inn.getItems()).extracting("quality").containsExactly(17, 4, 4, 80, 23, 3);
   }
 }
